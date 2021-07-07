516151d8594d763e76267c44a68cae53908cdbb6
wyzwzz <1076499338@qq.com>
2021-07-07 21:41:04 +0800

    update tf widget

--- apps/QTVolumeSlicer/src/TransferFunctionWidget.hpp
+++ apps/QTVolumeSlicer/src/TransferFunctionWidget.hpp
@@ -5,4 +5,15 @@
 #ifndef VOLUMESLICER_TRANSFERFUNCTIONWIDGET_HPP
 #define VOLUMESLICER_TRANSFERFUNCTIONWIDGET_HPP
 
+#include <QWidget>
+
+class TransferFunctionWidget: public QWidget{
+    Q_OBJECT
+public:
+    explicit TransferFunctionWidget(QWidget* parent=nullptr);
+
+private:
+
+};
+
 #endif //VOLUMESLICER_TRANSFERFUNCTIONWIDGET_HPP
