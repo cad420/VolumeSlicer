778c7ac3a9f4e7f4938414dcb8ffab05d77fc2a2
wyzwzz <1076499338@qq.com>
2021-08-04 17:38:29 +0800

    update volume setting widget

--- apps/QTVolumeSlicer/src/VolumeSettingWidget.cpp
+++ apps/QTVolumeSlicer/src/VolumeSettingWidget.cpp
@@ -24,31 +24,25 @@ QWidget(parent)
         auto volume_name_label=new QLabel("Comp Volume File");
         auto volume_name_line_edit=new QLineEdit();
         volume_name_line_edit->setReadOnly(true);
-        auto volume_name_button=new QPushButton(QIcon(
-                "C:\\Users\\wyz\\projects\\VolumeSlicer\\apps\\QTVolumeSlicer\\icons\\open.png"
-                ),"Load");
-        connect(volume_name_button,&QPushButton::clicked,[this](){
-            auto comp_volume_name=QFileDialog::getOpenFileName(this,
-                                         QStringLiteral("OpenCompFile"),
-                                         QStringLiteral("."),
-                                         QStringLiteral("comp files(*.h264)")
-            ).toStdString();
-        });
+
         volume_name_layout->addWidget(volume_name_label);
         volume_name_layout->addWidget(volume_name_line_edit);
-        volume_name_layout->addWidget(volume_name_button);
         comp_volume_layout->addLayout(volume_name_layout);
 
 
         auto volume_dim_layout=new QHBoxLayout();
         auto volume_dim_label=new QLabel("Comp Volume Dim");
-        auto volume_dim_x_label=new QLabel("0");
-        auto volume_dim_y_label=new QLabel("0");
-        auto volume_dim_z_label=new QLabel("0");
+        auto volume_dim_x_line_edit=new QLineEdit("0");
+        auto volume_dim_y_line_edit=new QLineEdit("0");
+        auto volume_dim_z_line_edit=new QLineEdit("0");
+        volume_dim_x_line_edit->setReadOnly(true);
+        volume_dim_y_line_edit->setReadOnly(true);
+        volume_dim_z_line_edit->setReadOnly(true);
+
         volume_dim_layout->addWidget(volume_dim_label);
-        volume_dim_layout->addWidget(volume_dim_x_label);
-        volume_dim_layout->addWidget(volume_dim_y_label);
-        volume_dim_layout->addWidget(volume_dim_z_label);
+        volume_dim_layout->addWidget(volume_dim_x_line_edit);
+        volume_dim_layout->addWidget(volume_dim_y_line_edit);
+        volume_dim_layout->addWidget(volume_dim_z_line_edit);
         comp_volume_layout->addLayout(volume_dim_layout);
 
         auto volume_space_layout=new QHBoxLayout();
@@ -61,7 +55,6 @@ QWidget(parent)
         volume_space_layout->addWidget(volume_space_y_line_edit);
         volume_space_layout->addWidget(volume_space_z_line_edit);
         comp_volume_layout->addLayout(volume_space_layout);
-
     }
 
 
@@ -73,30 +66,24 @@ QWidget(parent)
         auto volume_name_label=new QLabel("Raw Volume File");
         auto volume_name_line_edit=new QLineEdit();
         volume_name_line_edit->setReadOnly(true);
-        auto volume_name_button=new QPushButton(QIcon(
-                "C:\\Users\\wyz\\projects\\VolumeSlicer\\apps\\QTVolumeSlicer\\icons\\open.png"
-        ),"Load");
-        connect(volume_name_button,&QPushButton::clicked,[this](){
-            auto comp_volume_name=QFileDialog::getOpenFileName(this,
-                                                               QStringLiteral("OpenRawFile"),
-                                                               QStringLiteral("."),
-                                                               QStringLiteral("raw files(*.raw)")
-            ).toStdString();
-        });
+
         volume_name_layout->addWidget(volume_name_label);
         volume_name_layout->addWidget(volume_name_line_edit);
-        volume_name_layout->addWidget(volume_name_button);
         raw_volume_layout->addLayout(volume_name_layout);
 
         auto volume_dim_layout=new QHBoxLayout();
         auto volume_dim_label=new QLabel("Raw Volume Dim");
-        auto volume_dim_x_label=new QLabel("0");
-        auto volume_dim_y_label=new QLabel("0");
-        auto volume_dim_z_label=new QLabel("0");
+        auto volume_dim_x_line_edit=new QLineEdit("0");
+        auto volume_dim_y_line_edit=new QLineEdit("0");
+        auto volume_dim_z_line_edit=new QLineEdit("0");
+        volume_dim_x_line_edit->setReadOnly(true);
+        volume_dim_y_line_edit->setReadOnly(true);
+        volume_dim_z_line_edit->setReadOnly(true);
+
         volume_dim_layout->addWidget(volume_dim_label);
-        volume_dim_layout->addWidget(volume_dim_x_label);
-        volume_dim_layout->addWidget(volume_dim_y_label);
-        volume_dim_layout->addWidget(volume_dim_z_label);
+        volume_dim_layout->addWidget(volume_dim_x_line_edit);
+        volume_dim_layout->addWidget(volume_dim_y_line_edit);
+        volume_dim_layout->addWidget(volume_dim_z_line_edit);
         raw_volume_layout->addLayout(volume_dim_layout);
 
         auto volume_space_layout=new QHBoxLayout();
@@ -110,7 +97,8 @@ QWidget(parent)
         volume_space_layout->addWidget(volume_space_z_line_edit);
         raw_volume_layout->addLayout(volume_space_layout);
     }
-
+    comp_volume_group_box->setFixedHeight(200);
+    raw_volume_group_box->setFixedHeight(200);
     widget_layout->addWidget(comp_volume_group_box);
     widget_layout->addWidget(raw_volume_group_box);
     this->setLayout(widget_layout);
