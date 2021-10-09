b496528b28e8cdb72f4987214204e4021ebdc297
wyzwzz <1076499338@qq.com>
2021-10-09 15:57:14 +0800

    feat:add mark for global variable between dll and main program

--- src/Common/cuda_context.cpp
+++ src/Common/cuda_context.cpp
@@ -4,6 +4,11 @@
 #include <VolumeSlicer/cuda_context.hpp>
 #include <VolumeSlicer/singleton.hpp>
 
+// global variable between dll and main program
+//https://myprogrammingnotes.com/global-variable-main-program-conflict-global-variable-dll-name.html
+
+//using cuda driver api in different dlls should explict create cuda context in the dll
+//todo
 void SetCUDACtx(CUdevice d){
     Singleton<CUDACtx>::init(d);
 }
