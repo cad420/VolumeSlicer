2ba2429b50c08212716a73767afea2c6887c4197
wyzwzz <1076499338@qq.com>
2021-10-11 17:18:14 +0800

    feat:add volume value gen to H264 generator

--- tools/H264VolumeCDFGenerator.cpp
+++ tools/H264VolumeCDFGenerator.cpp
@@ -37,11 +37,13 @@ int main(int argc,char** argv){
 
     json j_chebyshev_dist;
     json j_average_value;//0-255
+    json j_volume_value;
 
     j_chebyshev_dist["cdf_block_length"]=cdf_block_length;
     j_chebyshev_dist["volume_block_length"]=block_length;
     j_average_value["cdf_block_length"] = cdf_block_length;
     j_average_value["volume_block_length"]=block_length;
+    j_volume_value["volume_block_length"]=block_length;
     auto index2string=[](const std::array<uint32_t,3>& index){
         return std::to_string(index[0])+"_"+std::to_string(index[1])+"_"+std::to_string(index[2]);
     };
@@ -60,14 +62,28 @@ int main(int argc,char** argv){
     }
     LOG_INFO("Total block calculate number: {0}.",tasks.size());
     using VolumeBlock = typename CompVolume::VolumeBlock;
-
     using CDFRetType1 = decltype(std::declval<CDFGenerator>().GetCDFArray());
     using CDFRetType2 = decltype(std::declval<CDFGenerator>().GetCDFValArray());
     using CDFRetType  = std::pair<CDFRetType1,CDFRetType2>;
-    auto cdf_gen = [block_length,cdf_block_length](VolumeBlock volume_block)->CDFRetType {
+    auto order=[](const std::array<uint32_t,4>& idx1,const std::array<uint32_t,4>& idx2){
+        if(idx1[3]==idx2[3]){
+            if(idx1[2]==idx2[2]){
+                if(idx1[1]==idx2[1]){
+                    return idx1[0]<idx2[0];
+                }
+                else return idx1[1]<idx2[1];
+            }
+            else return idx1[2]<idx2[2];
+        }
+        else return idx1[3]<idx2[3];
+    };
+    std::map<std::array<uint32_t,4>,int,decltype(order)> volume_value(order);
+    auto cdf_gen = [block_length,cdf_block_length,&volume_value](VolumeBlock volume_block)->CDFRetType {
       CDFGenerator cdf_generator;
       cdf_generator.SetVolumeBlockData(volume_block,block_length,cdf_block_length);
+      volume_value[volume_block.index] = cdf_generator.GetVolumeAvgValue();
       //in future should release VolumeBlock after copy not until finish generating cdf
+      //notice Release is not auto call in destruct function
       volume_block.Release();
       cdf_generator.GenerateCDF();
       return {cdf_generator.GetCDFArray(),cdf_generator.GetCDFValArray()};
@@ -85,9 +101,10 @@ int main(int argc,char** argv){
         results.reserve(parallel_num);
         while(i>0){
             auto volume_block = comp_volume->GetBlock();
+            auto index = volume_block.index;
             if(!volume_block.valid) continue;
             i--;
-            results.emplace_back(volume_block.index,thread_pool.AppendTask(cdf_gen,std::move(volume_block)));
+            results.emplace_back(index,thread_pool.AppendTask(cdf_gen,std::move(volume_block)));
         }
         for(auto& ret:results){
             auto lod =ret.first[3];
@@ -97,12 +114,17 @@ int main(int argc,char** argv){
             j_average_value[lod2string(lod)][index2string(index)] = cdf_ret.second;
         }
     }
+
     //chebyshev dist json
     try
     {
         std::ofstream out("chebyshev_dist_"+output_filename);
+        if(!out.is_open()){
+            throw std::runtime_error("Open output file for chebyshev dist failed.");
+        }
         out<<j_chebyshev_dist<<std::endl;
         out.close();
+        LOG_INFO("Finish writing chebyshev dist cdf config to json file.");
     }
     catch (std::exception const& err)
     {
@@ -112,13 +134,17 @@ int main(int argc,char** argv){
     {
         LOG_ERROR("Writing chebyshev dist result to file failed.");
     }
-    LOG_INFO("Finish writing chebyshev dist cdf config to json file.");
+
     //average value json
     try
     {
         std::ofstream out("average_value_"+output_filename);
+        if(!out.is_open()){
+            throw std::runtime_error("Open output file for average value failed.");
+        }
         out<<j_average_value<<std::endl;
         out.close();
+        LOG_INFO("Finish writing average value cdf config to json file.");
     }
     catch (std::exception const& err)
     {
@@ -128,5 +154,36 @@ int main(int argc,char** argv){
     {
         LOG_ERROR("Writing average value result to file failed.");
     }
-    LOG_INFO("Finish writing average value cdf config to json file.");
+
+    //block of volume value json
+    try{
+        std::unordered_map<uint32_t,std::vector<uint32_t>> value_array;
+        //volume value is sorted in the order same with flag virtual block index
+        for(auto& it:volume_value){
+            auto lod = it.first[3];
+            auto value = it.second;
+            value_array[lod].push_back(value);
+        }
+        std::ofstream out("volume_value_"+output_filename);
+        if(!out.is_open()){
+            throw std::runtime_error("Open output file for volume value failed.");
+        }
+        for(auto& it:value_array){
+            LOG_INFO("lod {0} has value size {1}.",it.first,it.second.size());
+            j_volume_value[lod2string(it.first)]=it.second;
+        }
+        out<<j_volume_value<<std::endl;
+        out.close();
+        LOG_INFO("Finish writing volume value config to json file.");
+    }
+    catch (std::exception const& err)
+    {
+        LOG_ERROR("Writing volume value json to file cause error: {0}.",err.what());
+    }
+    catch (...)
+    {
+        LOG_ERROR("Writing volume value result to file failed.");
+    }
+
+    return 0;
 }
