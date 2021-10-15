dc6a8dd619ce290eb5058cb22b6f8a1e2ed85db5
wyzwzz <1076499338@qq.com>
2021-10-15 16:50:01 +0800

    feat:add raw resource to window manager

--- apps/MPIVolumeSlicer/src/WindowManager.hpp
+++ apps/MPIVolumeSlicer/src/WindowManager.hpp
@@ -26,20 +26,40 @@ public:
         MPI_Get_processor_name(processName,&processNameLength);
         std::cout<<"world size is: "<<world_size<<", node "<<world_rank<<" is on host "<<processName<<std::endl;
 
-        world_window_width=j.at("width");
-        world_window_height=j.at("height");
-        row_num=j.at("row");
-        col_num=j.at("col");
-        auto space=j.at("space");
-        space_x=space.at(0);
-        space_y=space.at(1);
-        space_z=space.at(2);
+        try
+        {
+            world_window_width = j.at("width");
+            world_window_height = j.at("height");
+            row_num = j.at("row");
+            col_num = j.at("col");
+            auto space = j.at("space");
+            space_x = space.at(0);
+            space_y = space.at(1);
+            space_z = space.at(2);
 
-        auto screen_config=j.at("screen").at(std::to_string(world_rank));
-        screen_offset_x=screen_config.at("offsetX");
-        screen_offset_y=screen_config.at("offsetY");
-        resource_path=screen_config.at("resourcePath");
-        std::cout<<"node "<<world_rank<<" resource path: "<<resource_path<<std::endl;
+            auto screen_config = j.at("screen").at(std::to_string(world_rank));
+            screen_offset_x = screen_config.at("offsetX");
+            screen_offset_y = screen_config.at("offsetY");
+            resource_path = screen_config.at("resourcePath");
+            std::cout << "node " << world_rank << " resource path: " << resource_path << std::endl;
+
+            auto raw_config = j.at("raw");
+            raw_resource_path = raw_config.at("path");
+            raw_lod = raw_config.at("lod");
+            raw_dim_x = raw_config.at("dim")[0];
+            raw_dim_y = raw_config.at("dim")[1];
+            raw_dim_z = raw_config.at("dim")[2];
+        }
+        catch (std::exception const& err)
+        {
+            std::cout<<"WindowManager open file cause error: "<<err.what()<<std::endl;
+            exit(-1);
+        }
+        catch (...)
+        {
+            std::cout<<"WindowManager open file failed"<<std::endl;
+            exit(-1);
+        }
     }
     ~WindowManager(){
         MPI_Finalize();
@@ -88,8 +108,24 @@ public:
     std::string GetNodeResourcePath() const{
         return resource_path;
     }
+    bool IsRoot(int rank) const{
+        return rank == root_rank;
+    }
+    bool IsRoot() const{
+        return world_rank == root_rank;
+    }
+    std::string GetRawResourcePath(int& lod) const{
+        lod=raw_lod;
+        return raw_resource_path;
+    }
+    void GetRawDim(int& dim_x,int& dim_y,int& dim_z) const{
+        dim_x=raw_dim_x;
+        dim_y=raw_dim_y;
+        dim_z=raw_dim_z;
+    }
 public:
     //same for each node
+    int root_rank = 0;
     int world_window_width;
     int world_window_height;
     int world_size;
@@ -103,6 +139,10 @@ public:
     int screen_offset_x;
     int screen_offset_y;
     std::string resource_path;
+
+    std::string raw_resource_path;
+    int raw_lod;
+    int raw_dim_x,raw_dim_y,raw_dim_z;
 };
 
 #endif //VOLUMESLICER_WINDOWMANAGER_HPP
