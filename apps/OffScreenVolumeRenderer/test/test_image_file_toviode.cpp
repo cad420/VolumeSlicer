496eec02ae9b1c55387be69e99a85a92773d824d
wyzwzz <1076499338@qq.com>
2021-10-25 17:24:56 +0800

    feat:add some files

---
+++ apps/OffScreenVolumeRenderer/test/test_image_file_toviode.cpp
@@ -0,0 +1,21 @@
+//
+// Created by wyz on 2021/10/18.
+//
+#include <VolumeSlicer/render.hpp>
+#include "VideoCapture.hpp"
+#include <iostream>
+#include <filesystem>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;
+int main(){
+    ::VideoCapture vc("test_img_file_tovideo.avi",1200,900,30);
+    for(int i=0;i<10;i++){
+        std::string img_name="images/result333_frame_"+std::to_string(i)+".jpeg";
+        auto img = imread(img_name);
+
+        vc.AddFrame(img.data);
+    }
+
+    return 0;
+}
